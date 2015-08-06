05358f23cb3c3032953d5344f192dae3bd79901d
viqtus <viqtus@gmail.com>
2015-08-06 14:18:30 +0300

    2015.3

--- index.js
+++ index.js
@@ -20,17 +20,24 @@ var game =
 
 	},
 
+	mode: 'menu',
+
 	update: function()
 	{
-		
+
 	}
 };
+
+var image = new Image();
+	image.src = 'selector.png';
 
 window.document.body.appendChild(canvas);
 
 window.onload = function()
 {
 	game.canvas.resize();
+	context.drawImage(image, canvas.width/2 - image.width/2, canvas.height/2 - image.height/2, image.width, image.height);
+	context.scale(2, 2);
 };
 
 window.onresize = function()
