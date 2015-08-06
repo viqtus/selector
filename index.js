b3a851c240d06d6060f83c51b86ed351751a6520
viqtus <viqtus@gmail.com>
2015-08-06 13:02:32 +0300

    2015.1

---
+++ index.js
@@ -0,0 +1,7 @@
+var window = window;
+var canvas = window.document.createElement('canvas');
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+var context = canvas.getContext('2d');
+
+window.document.body.appendChild(canvas);
