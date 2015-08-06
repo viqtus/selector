be8aad8e472ed3346d210284eec827a1feae1cf4
viqtus <viqtus@gmail.com>
2015-08-06 13:17:53 +0300

    2015.2

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 var window = window;
+
 var canvas = window.document.createElement('canvas');
-	canvas.height = window.innerHeight;
-	canvas.width = window.innerWidth;
+
 var context = canvas.getContext('2d');
 
+var game =
+{
+	canvas:
+	{
+		resize: function()
+		{
+			canvas.height = window.innerHeight;
+			canvas.width = window.innerWidth;
+		}
+	},
+
+	draw: function()
+	{
+
+	},
+
+	update: function()
+	{
+		
+	}
+};
+
 window.document.body.appendChild(canvas);
+
+window.onload = function()
+{
+	game.canvas.resize();
+};
+
+window.onresize = function()
+{
+	game.canvas.resize();
+};
